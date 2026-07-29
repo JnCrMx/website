e76e5ebc803e92113498e49f5d0d0ff09e2e17ee
JCM <jcm@jcm.re>
2026-07-29 05:53:27 +0200

    add external images as local assets
    this prevents requests to domains I don't own

--- src/main.cpp
+++ src/main.cpp
@@ -73,17 +73,17 @@ namespace windows {
             p{"I'm a software developer and computer engineering student."},
             p{{_class{"socials"}},
                 a{{_href{"https://github.com/JnCrMx/"}, _target{"_blank"}},
-                    img{{_src{"https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"}, _title{"GitHub"}}}},
+                    img{{_src{"images/third_party/github.png"}, _title{"GitHub"}}}},
                 a{{_href{"https://git.jcm.re/jcm/"}, _target{"_blank"}},
-                    img{{_src{"https://git.jcm.re/assets/img/logo.svg"}, _title{"Forgejo instance"}}}},
+                    img{{_src{"images/third_party/githug.svg"}, _title{"Forgejo instance"}}}},
                 a{{_href{"https://bsky.app/profile/jcm.re"}, _target{"_blank"}},
-                    img{{_src{"https://web-cdn.bsky.app/static/apple-touch-icon.png"}, _title{"Bluesky"}}}},
+                    img{{_src{"images/third_party/bluesky.png"}, _title{"Bluesky"}}}},
                 a{{_href{"https://wafrn.jcm.re/blog/jcm"}, _target{"_blank"}},
-                    img{{_src{"https://wafrn.jcm.re/assets/icons/icon-512x512.png"}, _title{"Wafrn"}}}},
+                    img{{_src{"images/third_party/wafrn.png"}, _title{"Wafrn"}}}},
                 a{{_href{"https://blog.jcm.re/"}, _target{"_blank"}},
-                    img{{_src{"https://files.jcm.re/blog_icon.png"}, _title{"Blog"}}}},
+                    img{{_src{"images/third_party/blog.png"}, _title{"Blog"}}}},
                 a{{_href{"https://keys.openpgp.org/vks/v1/by-fingerprint/7B839F0DA64069DD7832BB802F536DE3E9EC3AD8"}, _target{"_blank"}},
-                    img{{_src{"https://www.openpgp.org/images/apple-touch-icon.png"}, _title{"OpenPGP"}}}},
+                    img{{_src{"images/third_party/openpgp.png"}, _title{"OpenPGP"}}}},
             },
             p{"P.S.: I love Cyndi~! 🩷🩵"},
         };
@@ -106,12 +106,12 @@ namespace windows {
             "OpenStore",
         };
         static constexpr std::array link_type_icons = {
-            "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png",
-            "https://git.jcm.re/assets/img/logo.svg",
-            "https://upload.wikimedia.org/wikipedia/commons/2/22/Document_%2889366%29_-_The_Noun_Project.svg",
-            "https://woodpecker.jcm.re/favicons/favicon-light-default.svg",
-            "https://snapcraft.io/static/snapcraft-favicon.png",
-            "https://open-store.io/logo.png",
+            "images/third_party/github.png",
+            "images/third_party/githug.svg",
+            "images/third_party/documentation.svg",
+            "images/third_party/woodpecker.svg",
+            "images/third_party/snapcraft.png",
+            "images/third_party/open-store.png",
         };
 
         struct ProjectLink : public component<ProjectLink> {
